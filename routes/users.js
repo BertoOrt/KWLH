cea9cccb97cef87c3a31bdc23484a3757585bfcb
Roberto Ortega <berto.ort@gmail.com>
2015-07-09 14:22:34 -0600

    css beautiful

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:username', function(req, res, next) {
-  res.render('users/index', { title: 'Know/What/Learn/How' });
+  res.render('users/index', { title: 'Know/What/Learn/How', username: req.params.username });
 });
 
 router.get('/:username/new', function(req, res, next) {
-  res.render('users/new', { title: 'New' });
+  res.render('users/new', { title: 'New', username: req.params.username });
 });
 
-router.get('/:username/edit', function(req, res, next) {
-  res.render('users/edit', { title: 'Edit' });
+router.post('/new', function(req, res, next) {
+  res.redirect('/' + req.session.username)
+});
+
+router.get('/:username/:chart/edit', function(req, res, next) {
+  res.render('users/edit', { title: 'Edit', username: req.params.username });
+});
+
+router.get('/:username/:chart', function(req, res, next) {
+  res.render('users/show', { title: req.params.chart, username: req.params.username });
 });
 
 module.exports = router;
