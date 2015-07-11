f0b05e55ec1d362d79f9f66fd41fe97e3b361ac2
Steven Abrams <steven.abrams86@gmail.com>
2015-07-11 17:45:04 -0600

    added confirm to delete chart

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
   $("#markdown").markdown({autofocus:false,savable:false})
+  //check before deleting a chart
+  $('.delete-chart').click(function () {
+    var confirmation = window.confirm('Are you sure you want to delete this KWL(H)? Information cannot be recovered');
+    if (confirmation){
+      $('.delete-user-form').submit();
+    } else {
+      event.preventDefault();
+    }
+  });
+
 });
