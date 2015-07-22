85467c2d9e6aacaa3043894f62115886888990bc
Roberto Ortega <berto.ort@gmail.com>
2015-07-21 22:49:09 -0600

    marking

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {
-  $("#markdown").markdown({autofocus:false,savable:false})
+  $(".mark").on('click', function (e) {
+    var textarea = e.target.parentNode.firstChild;
+    var preview = e.target.parentNode.children[1];
+    var text = textarea.value;
+    preview.innerHTML = markdown.toHTML(text);
+    if (textarea.style.display === 'none') {
+      textarea.style.display = 'block';
+      preview.style.display = 'none';
+    }
+    else {
+      textarea.style.display = 'none';
+      preview.style.display = 'block';
+    }
+  })
   //check before deleting a chart
   $('.delete-chart').click(function () {
     var confirmation = window.confirm('Are you sure you want to delete this KWL(H)? Information cannot be recovered');
